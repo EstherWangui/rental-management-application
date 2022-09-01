b1b4854e8b51a0b85988c1215a2d91125ce8feba
EstherWangui <esther.nganga@student.moringaschool.com>
2022-09-01 14:24:36 +0300

    commit changes

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 
-const random foods
-
+const randomSee = document.getElementById("see");
+const display = document.getElementById("display")
+randomSee.addEventListener('click',randomPics);
+let random
+function randomPics(){
+  fetch('https://realtymole-rental-estimate-v1.p.rapidapi.com/rentalPrice?address=5500%20Grand%20Lake%20Drive%2C%20San%20Antonio%2C%20TX%2C%2078244&propertyType=Single%20Family&bedrooms=4&bathrooms=2&squareFootage=1600&compCount=5')
+	.then((response )=> response.json())
+	.then((data) => random = data.image);
+	
+  display.innerHTML="<img src={data.image width=\"400px\" height=\"150px\">";
+}
 let flataTitle = document.getElementById("card-title");
 let flataImage = document.getElementById("card-image");
 let flataLikes = document.querySelector(".likes-section");
