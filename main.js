6667adc83e20357ed132ff745b042d9b6da6f2b3
EstherWangui <esther.nganga@student.moringaschool.com>
2022-09-03 09:38:47 +0300

    modified db.json

--- main.js
+++ main.js
@@ -13,9 +13,8 @@ function randomPics(){
 	.then((data) => {
 	console.log(data)
 
-  display.innerHTML="<img src="+data[0,2,3,4].image+" width='350px' height='270px'/>";
-  display.innerHTML="<img src="+data[1].image+" width='350px' height='270px'/>";
-  display.innerHTML="<img src="+data[2].image+" width='350px' height='270px'/>";
+  display.innerHTML="<img src="+data[0,1.2,3,4].image+" width='350px' height='270px'/>";
+  
 
 });
 } 
